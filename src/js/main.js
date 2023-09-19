14fbb5a35093da8856e8ec58b8705e6e1fd52dbc
Nicolas-S-Linhares <nicolassezerinolinhares@gmail.com>
2023-09-19 17:30:27 -0300

    feat = construindo script

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,24 @@
-const cep= document.querySelector("#cep")
+const cep = document.querySelector("#cep")
+
+const showData = (result)=>{
+    for(const campo in result){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = 
+        }
+    }
+}
 
 cep.addEventListener("blur",(e)=>{
-    console.log(cep.value)
+    let search = cep.value.replace("-","")
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`http://viacep.com.br/ws/${search}/json/`, options)
+    .then(response =>{ response.json()
+    .catch( data => console.log(data))
+})
+.catch(e => console.log('Deu Erro: '+ e, message))
 })
